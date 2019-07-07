631b33179353d484928b2fbc04b5a33b270be7b3
Pooja <52634839+Pooja2601@users.noreply.github.com>
2019-07-08 01:13:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './component/Login'
+import Navigate from './Navigate'
+import fire from './config/Fire'
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
+  
+  
+  render() {
+  return (
+      
+      <div className="App">
+     {this.state.user ? (<Navigate/>):(<Login/>)}
+    </div>
+     
+    );
+  }
+}
+
+export default App;
